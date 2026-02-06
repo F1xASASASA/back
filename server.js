7d715fd82c1e14022ec2fc9dba1caaf111049195
F1xASASASA <f1xasasasa@gmail.com>
2026-02-06 15:47:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ let globalCharacters = [
     name: 'Геральт', 
     author: '@system', 
     desc: 'Ведьмак из Ривии', 
-    avatar: 'https://i.imgur.com/3p8WnID.png', 
+    avatar: 'https://images.steamusercontent.com/ugc/924805934474316745/E44AC605B446C8420FF450DDDEB428BC89D16BC8/', 
     prompt: 'Ты Геральт из Ривии. Твоя речь скупая и циничная. Используй "Зараза", "Хмм".' 
   }
 ];
@@ -156,3 +156,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
+
